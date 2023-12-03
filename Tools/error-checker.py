041ac01d8496abd8f3328dbe8f32cb3d48121a3f
jayson1200 <jaysonmeribe2004@gmail.com>
2023-12-03 02:41:11 -0800

    stuff

--- Tools/error-checker.py
+++ Tools/error-checker.py
@@ -1,15 +1,26 @@
 import os
 import codecs
+import numpy as np
 
-def print_file_contents(root_dir):
-    for root, dirs, files in os.walk(root_dir):
-        for file in files:
-            file_path = os.path.join(root, file)
-            try:
-                with codecs.open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
-                    print(f"Contents of {file_path}:")
-                    print(f.read())
-            except Exception as e:
-                print(f"Could not read file {file_path} due to {str(e)}")
+root_dir = f"/home/meribejayson/Desktop/Projects/trailblazer/partial-joint"
+problem_paths = []
 
-print_file_contents('../partial-joint')
+for root, dirs, files in os.walk(root_dir):
+    for file in files:
+        file_path = os.path.join(root, file)
+        try:
+            with codecs.open(file_path, 'r', encoding='utf-8', errors='ignore') as f:   
+                prob_list = np.array(f.read().split("\n"), dtype = float)
+                sum_prob = np.sum(prob_list)
+
+                if(prob_list.len != 5):
+                    problem_paths.append("List length was greater than 5 at: \n" +file_path + "\n")
+                elif(np.abs(sum_prob - 1) > 0.02 or sum_prob == 0):
+                    problem_paths.append("False probability values: \n" +file_path + "\n")
+
+        except Exception as e:
+            problem_paths.append("Couldn't Read File: \n" +file_path + "\n")
+
+
+for path in problem_paths:
+    print(path + "\n")
