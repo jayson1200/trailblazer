862310181a112df2fc23b81b68337acfdb3eaa22
jayson1200 <jaysonmeribe2004@gmail.com>
2023-12-03 04:32:03 -0800

    stuff

---
+++ Tools/class-prob.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np
+
+root_dir = f"/home/meribejayson/Desktop/Projects/trailblazer/partial-joint"
+problem_paths = []
+
+with open("class-prob", "x+") as file_write:
+    with open("class-prob", 'w') as file_write:
+        for root, dirs, files in os.walk(root_dir):
+            for dir in dirs:
+                if(dir != "ATHLETIC" and dir != "CS" and dir != "OUTDOOR" and dir != "PHYSWELL"):
+                    file_write.write(dir + "," + "\n")
--- Tools/error-checker.py
+++ Tools/error-checker.py
@@ -1,5 +1,4 @@
 import os
-import codecs
 import numpy as np
 
 root_dir = f"/home/meribejayson/Desktop/Projects/trailblazer/partial-joint"
@@ -9,17 +8,17 @@ for root, dirs, files in os.walk(root_dir):
     for file in files:
         file_path = os.path.join(root, file)
         try:
-            with codecs.open(file_path, 'r', encoding='utf-8', errors='ignore') as f:   
+            with open(file_path, 'r') as f:   
                 prob_list = np.array(f.read().split("\n"), dtype = float)
                 sum_prob = np.sum(prob_list)
 
-                if(prob_list.len != 5):
-                    problem_paths.append("List length was greater than 5 at: \n" +file_path + "\n")
+                if(prob_list.size != 5):
+                    problem_paths.append("List length was not equal to 5 at: \n" + file_path + "\n")
                 elif(np.abs(sum_prob - 1) > 0.02 or sum_prob == 0):
-                    problem_paths.append("False probability values: \n" +file_path + "\n")
+                    problem_paths.append("False probability values: \n" + file_path + "\n")
 
         except Exception as e:
-            problem_paths.append("Couldn't Read File: \n" +file_path + "\n")
+            problem_paths.append(str(e) + " at: \n" + file_path + "\n")
 
 
 for path in problem_paths:
